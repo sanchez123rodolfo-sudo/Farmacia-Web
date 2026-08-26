21af59eb9b842ed1e86c3c7d7e5d85dc8d674d5c
Eduardo <sanchez123rodolfo@gmail.com>
2026-08-25 22:19:33 -0500

    Limpieza total y definitiva con gitignore ordenado

--- ordenar_lista.py
+++
@@ -1,8 +0,0 @@
-def ordenar_lista(lista):
-    return sorted(lista)
-
-
-if __name__ == "__main__":
-    lista = [5, 2, 9, 1, 7, 3]
-    print("Original:", lista)
-    print("Ascendente:", ordenar_lista(lista))
